7a6bfaea645f682b6492fcf7203c93ccfe915ec6
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2022-01-20 19:25:22 +0900

    Added links

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
 	let name = 'world';
+  const links = [{
+    name: 'Instagram', link: 'https://www.instagram.com/KmanJP/'
+  },{
+    name: 'Twitter', link: 'https://twitter.com/Kman_JP'
+  },{
+    name: 'Twitch', link: 'https://www.twitch.tv/kmanjp'
+  },{
+    name: 'Blog', link: 'http://blog.kevinadu.com/'
+  },{
+    name: 'Photography', link: 'https://www.shutterstock.com/g/kevinadu?rid=197988622&utm_medium=email&utm_source=ctrbreferral-link'
+  },
+  {
+    name: 'Soundcloud', link: 'https://soundcloud.com/kmanjp'
+  }]
 </script>
 
-<h1>Hello {name}!</h1>
+{#each links as {name, link}}
+  <a href={link}>
+    <p>{name}</p>
+  </a>
+{/each}
